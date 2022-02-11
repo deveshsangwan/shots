f2c0c209baba89db43599e533bbdc212fdba7bd5
Devesh Sangwan <dev.sangwan2001@gmail.com>
2022-02-11 20:06:58 +0530

    updated main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/*
- Multiverse by HTML5 UP
- html5up.net | @ajlkn
- Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-
- Added EXIF data and enhanced for Jekyll by Ram Patra
- */
 
 (function ($) {
 
